2f60ce738bd363ad45574e532f301ac50dca1ce7
Axel Espinosa <caer23.voltr@outlook.com>
2026-05-28 22:42:08 -0600

    Replaced all errors with ConstructionError so it can be identified when an error occurs

--- core/interpolation/splines.py
+++ core/interpolation/splines.py
@@ -2,7 +2,7 @@ import pandas as pd
 from typing import Dict, Any
 import numpy as np
 from sympy import sympify, symbols
-from core.exceptions import ValidationError
+from core.exceptions import ConstructionError
 
 
 class SplineConstructor():
@@ -15,15 +15,15 @@ class SplineConstructor():
         self.mode = input_data.get("mode")
 
         if self.mode != "table":
-            raise ValueError("Spline interpolation only supports 'table' mode.")
+            raise ConstructionError("Spline interpolation only supports 'table' mode.")
 
         self.df = input_data.get("data")
 
         if not isinstance(self.df, pd.DataFrame):
-            raise ValueError("Data must be a pandas DataFrame.")
+            raise ConstructionError("Data must be a pandas DataFrame.")
 
         if self.df.shape[1] != 2:
-            raise ValueError("DataFrame must have exactly 2 columns: x and f(x).")
+            raise ConstructionError("DataFrame must have exactly 2 columns: x and f(x).")
 
         self.df = self.df.sort_values(by=self.df.columns[0]).reset_index(drop=True)
         self.xk = input_data.get("xk")
