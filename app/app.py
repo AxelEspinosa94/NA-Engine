998c38e97595dfd7470485ad47446f8ccc2831a3
Axel Espinosa <caer23.voltr@outlook.com>
2026-07-05 21:55:20 -0600

    Registered integration callbacks

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from .layout.base_layout import layout
 from .callbacks.theme_callbacks import register_theme_callbacks
 from .callbacks.navigation_callbacks import register_navigation_callbacks
 from .callbacks.interpolation_callbacks import register_interpolation_callbacks
+from .callbacks.integration_callbacks import register_integration_callbacks
 
 def create_app():
     app = Dash(__name__, suppress_callback_exceptions=True, assets_folder="./assets")
@@ -12,5 +13,6 @@ def create_app():
     register_theme_callbacks(app)
     register_navigation_callbacks(app)
     register_interpolation_callbacks(app)
+    register_integration_callbacks(app)
 
     return app
