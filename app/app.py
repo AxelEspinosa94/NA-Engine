5c77ea805029eeff132cabefc9dbeddd33e14e90
Axel Espinosa <caer23.voltr@outlook.com>
2026-07-14 07:44:56 -0600

    Added numerical derivative callbacks in the register

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from .callbacks.navigation_callbacks import register_navigation_callbacks
 from .callbacks.interpolation_callbacks import register_interpolation_callbacks
 from .callbacks.integration_callbacks import register_integration_callbacks
 from .callbacks.linear_algebra_callbacks import register_linear_algebra_callbacks
+from .callbacks.numerical_derivative_callbacks import register_derivative_callbacks
 
 def create_app():
     app = Dash(__name__, suppress_callback_exceptions=True, assets_folder="./assets")
@@ -16,5 +17,6 @@ def create_app():
     register_interpolation_callbacks(app)
     register_integration_callbacks(app)
     register_linear_algebra_callbacks(app)
+    register_derivative_callbacks(app)
 
     return app
