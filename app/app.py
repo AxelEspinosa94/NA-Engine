a2587c950cdf224df0c501cccd8332a6ae4cd7ae
Axel Espinosa <caer23.voltr@outlook.com>
2026-07-20 13:27:56 -0600

    Registered documentation section callbacks

--- app/app.py
+++ app/app.py
@@ -8,6 +8,8 @@ from .callbacks.linear_algebra_callbacks import register_linear_algebra_callback
 from .callbacks.numerical_derivative_callbacks import register_derivative_callbacks
 from .callbacks.non_linear_callbacks import register_nonlinear_callbacks
 from .callbacks.ode_callbacks import register_ode_callbacks
+from .callbacks.docs_callbacks import register_docs_callbacks
+
 
 def create_app():
     app = Dash(__name__, suppress_callback_exceptions=True, assets_folder="./assets")
@@ -22,5 +24,6 @@ def create_app():
     register_derivative_callbacks(app)
     register_nonlinear_callbacks(app)
     register_ode_callbacks(app)
+    register_docs_callbacks(app)
 
     return app
