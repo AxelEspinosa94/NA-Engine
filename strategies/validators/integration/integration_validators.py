3fc932ac544ebdaca965cdd41258cca5da6fdbc5
Axel Espinosa <caer23.voltr@outlook.com>
2026-08-19 06:49:15 -0600

    (feature): Implementation of catalog approach for the module Integration.

---
+++ strategies/validators/integration/integration_validators.py
@@ -0,0 +1,32 @@
+import os
+
+from core.exceptions import ValidationError
+from app.utils.rule_loader import load_rule
+from app.utils.catalog_loader import load_catalog
+
+class IntegrationValidator:
+
+    def __init__(self):
+        base = os.path.dirname(__file__)
+        path = os.path.join(base, "integration_validation_catalog.json")
+        self.catalog = load_catalog(path)
+
+    def validate(self, input_data):
+        mode = input_data.get("mode")
+        calculation_mode = input_data.get("calculation_mode")
+
+        if mode != "function":
+            raise ValidationError("Integration only supports mode='function'.")
+
+        if calculation_mode not in self.catalog.get("supported_modes", []):
+            raise ValidationError("Unsupported calculation_mode.")
+
+        rules = self.catalog.get(calculation_mode).get("rules")
+        if isinstance(rules, str):#rule can be a single string or a list of strings
+            rules = [rules]  # Convert to list for uniform processing
+        for r in rules:
+            # Load the custom validation rule
+            validation_rule = load_rule(r)
+            validation_rule(input_data)
+
+        return True
---
+++ strategies/validators/integration/rules/common_rules.py
@@ -0,0 +1,23 @@
+from core.exceptions import ValidationError
+
+def _validate_n_for_mode(input_data):
+        mode = input_data.get("calculation_mode")
+        n = input_data.get("n")
+
+        rules = {
+            "trapezoid_simple": lambda n: n == 1,
+            "trapezoid_composite": lambda n: n >= 1,
+            "simpson_1_3": lambda n: n % 2 == 0,
+            "simpson_3_8": lambda n: n % 3 == 0,
+            "romberg": lambda n: True,
+            "gauss": lambda n: True,
+        }
+
+        if not rules[mode](n):
+            messages = {
+                "trapezoid_simple": "Trapezoid simple requires n = 1.",
+                "trapezoid_composite": "Trapezoid composite requires n >= 1.",
+                "simpson_1_3": "Simpson 1/3 requires even n.",
+                "simpson_3_8": "Simpson 3/8 requires n multiple of 3.",
+            }
+            raise ValidationError(messages.get(mode, "Invalid n."))
